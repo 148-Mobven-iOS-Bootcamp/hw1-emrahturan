762c7ef466252e9c39dae3d40fcc29a502ac61e7
Emrah Turan <emrah.turan@softtech.com.tr>
2021-12-18 02:06:21 +0300

    completed

--- hw1-emrahturan.playground/Contents.swift
+++ hw1-emrahturan.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var greeting = "Hello, playground"
+private func calcFibonacci(n: Int) -> Int {
+    return n <= 1 ? n : (calcFibonacci(n: n - 1) + calcFibonacci(n: n - 2))
+}
+
+
+func Fibonacci(startValue: Int, endValue: Int) {
+    var index:Int = 0
+    var value:Int = 0
+    
+    repeat {
+        value = calcFibonacci(n: index)
+        
+        if value >= endValue {
+            break
+        }
+        
+        if startValue <= value && value <= endValue {
+            print(value)
+        }
+        
+        index += 1
+    } while true
+}
+
+Fibonacci(startValue: 750, endValue: 1000)
+
+
